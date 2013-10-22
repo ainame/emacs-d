1f07ce7542218b5ae20de8759488aa93a97fce3c
satoshi.namai <satoshi.namai@mixi.co.jp>
2013-10-23 00:13:10 +0900

    rename init file

--- src/el-get-packages-conf/init-browser-kill-ring.el
+++ src/el-get-packages-conf/init-browser-kill-ring.el
@@ -1,4 +1,4 @@
-;; (setq browse-kill-ring-quit-action 'kill-and-delete-window)
+(setq browse-kill-ring-quit-action 'kill-and-delete-window)
 (setq browse-kill-ring-separator "-------")
 (setq browse-kill-ring-highlight-current-entry t)
 (custom-set-variables
