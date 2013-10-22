d782a1edadd5d4b1e06ab48c980df06371fab6fc
ainame <s.namai.2012@gmail.com>
2013-10-22 09:38:32 +0900

    fix

--- src/personal-conf/motion-mode.conf.el
+++ src/personal-conf/motion-mode.conf.el
@@ -1,4 +1,7 @@
+(require 'motion-mode)
 (add-to-list 'ac-modes 'motion-mode)
 (add-hook 'ruby-mode-hook 'motion-recognize-project)
 (define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
 (define-key motion-mode-map (kbd "C-c C-o") 'motion-dash-at-point)
+
+
