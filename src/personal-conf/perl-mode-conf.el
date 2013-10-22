39dfcda1e8a74434b24f50f7096f369279eda8e5
satoshi.namai <satoshi.namai@mixi.co.jp>
2013-10-22 22:57:31 +0900

    refine cperl-mode configuration

--- src/personal-conf/perl-mode-conf.el
+++ src/personal-conf/perl-mode-conf.el
@@ -1,27 +1,4 @@
 (defalias 'perl-mode 'cperl-mode)
-(setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
-(add-hook 
- 'cperl-mode-hook
- '(lambda ()
-    (cperl-set-style "PerlStyle")
-    (define-key cperl-mode-map (kbd "C-c C-c") 'quickrun)
-    (define-key cperl-mode-map (kbd "C-h") 'delete-backward-char)
-    (define-key cperl-mode-map (kbd "C-j") 'ace-jump-mode)
-    (local-unset-key "\C-h")
-    (set-face-background 'cperl-array-face "black")
-    (set-face-background 'cperl-hash-face "black")
-    (setq cperl-close-paren-offset -4)
-    (setq cperl-comment-column 40)
-    (setq cperl-continued-statement-offset 4)
-    (setq cperl-electric-keywords nil)
-    (setq cperl-electric-lbrace-space t)
-    (setq cperl-electric-parens nil)
-    (setq cperl-hairy t)
-    (setq cperl-indent-parens-as-block t)
-    (setq indent-tabs-mode nil)
-    (setq-default tab-width 4 indent-tabs-mode nil)
-    ))
-
 (setq auto-mode-alist (append '(("\\.pl$" . cperl-mode)) auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.psgi$" . cperl-mode)) auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.cgi$" . cperl-mode)) auto-mode-alist))
@@ -46,3 +23,28 @@
         (replace-regexp-in-string
          "[\s\n\r]+$" ""
          (shell-command-to-string (concat "perldoc -lm " module " 2> /dev/null"))))))))
+
+(add-hook 
+ 'cperl-mode-hook
+ '(lambda ()
+    (cperl-set-style "PerlStyle")
+    (define-key cperl-mode-map (kbd "C-c C-c") 'quickrun)
+    (define-key cperl-mode-map (kbd "C-h") 'delete-backward-char)
+    (define-key cperl-mode-map (kbd "C-j") 'ace-jump-mode)
+    (define-key cperl-mode-map (kbd "M-.") 'perl-find-module)
+    (local-unset-key "\C-h")
+    (set-face-background 'cperl-array-face "black")
+    (set-face-background 'cperl-hash-face "black")
+    (setq cperl-auto-newline nil)
+    (setq cperl-close-paren-offset -4)
+    (setq cperl-comment-column 40)
+    (setq cperl-continued-statement-offset 4)
+    (setq cperl-electric-keywords nil)
+    (setq cperl-electric-lbrace-space t)
+    (setq cperl-electric-parens nil)
+    (setq cperl-hairy t)
+    (setq cperl-indent-parens-as-block t)
+    (setq indent-tabs-mode nil)
+    (setq-default tab-width 4 indent-tabs-mode nil)
+    ))
+
