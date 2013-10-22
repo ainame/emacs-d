a2ce9500e9db503c65c67511243e02700c2f982b
ainame <s.namai.2012@gmail.com>
2013-10-22 09:32:07 +0900

    add initialization file of motion-mode

---
+++ src/el-get-packages-conf/init-motion-mode.el
@@ -0,0 +1,4 @@
+(add-to-list 'ac-modes 'motion-mode)
+(add-hook 'ruby-mode-hook 'motion-recognize-project)
+(define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
+(define-key motion-mode-map (kbd "C-c C-o") 'motion-dash-at-point)
