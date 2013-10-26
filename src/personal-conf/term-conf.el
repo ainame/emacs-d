6e715f2add87459e76fb87efe1e688f323aeda0c
ainame <s.namai.2012@gmail.com>
2013-10-26 13:19:35 +0900

    add comment for when using term

--- src/personal-conf/term-conf.el
+++ src/personal-conf/term-conf.el
@@ -5,6 +5,8 @@
 (setq explicit-shell-file-name shell-file-name)
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; write `export TERM=xterm-256color` on ~/.zshrc 
 (setq system-uses-terminfo nil)
 
 (global-set-key (kbd "C-c t")
