fe1388de22f2a779e0f55a67ded66df3e5f357b7
ainame <s.namai.2012@gmail.com>
2013-10-27 18:32:57 +0900

    fix browse-kill-ring-quit-action

--- src/el-get-packages-conf/init-browse-kill-ring.el
+++ src/el-get-packages-conf/init-browse-kill-ring.el
@@ -1,5 +1,5 @@
 (setq browse-kill-ring-quit-action 'kill-and-delete-window)
 (setq browse-kill-ring-separator "-------")
 (setq browse-kill-ring-highlight-current-entry t)
-(custom-set-variables '(browse-kill-ring-quit-action (quote bury-buffer)))
+(custom-set-variables '(browse-kill-ring-quit-action (quote kill-and-delete-window)))
 (global-set-key (kbd "M-y") 'browse-kill-ring)
