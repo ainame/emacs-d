30d6eb14e400e21a5b53aa0b21706717681a7ee5
satoshi.namai <satoshi.namai@mixi.co.jp>
2013-10-29 15:21:26 +0900

    remove enviroment.el from repository

--- src/init/environment.el
+++
@@ -1,6 +0,0 @@
-(defvar my-migemo-dir "/usr/local/share/migemo/utf-8/migemo-dict")
-(eval-after-load "term+"
-  '(progn
-     (setq shell-file-name (executable-find "zsh"))))
-
-(provide 'my-setup/environment)
