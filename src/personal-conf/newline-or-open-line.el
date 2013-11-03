54d7652bf7f543882b033b648ad3526a1c70727d
ainame <s.namai.2012@gmail.com>
2013-11-04 02:03:00 +0900

    update specific

--- src/personal-conf/newline-or-open-line.el
+++ src/personal-conf/newline-or-open-line.el
@@ -1,7 +1,9 @@
 (defun newline-or-open-line ()
   "newline-or-openline is a new command for merging C-m and C-o"
   (interactive)
-  (cond ((bolp) (progn (open-line 1) (indent-according-to-mode)))
-	((eolp) (newline))
+  (cond ((or (eolp)
+	     (not (string-match "[^\\\s\\\n\\\t]" (buffer-substring (point) (point-at-eol)))))
+	 (progn (newline) (indent-according-to-mode)))
 	(t (progn (open-line 1) (indent-according-to-mode)))))
+
 (define-key global-map (kbd "C-m") 'newline-or-open-line)
