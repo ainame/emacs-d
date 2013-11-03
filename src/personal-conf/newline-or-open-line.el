4234458a84dc56894b3d3710b4d21efb9900ca12
ainame <s.namai.2012@gmail.com>
2013-11-04 05:12:37 +0900

    align indent

--- src/personal-conf/newline-or-open-line.el
+++ src/personal-conf/newline-or-open-line.el
@@ -2,11 +2,11 @@
   "newline-or-openline is a new command for merging C-m and C-o"
   (interactive)
   (let ((string-exists-before-cursor (string-match "[^\\\s\\\n\\\t]" (buffer-substring (point-at-bol) (point))))
-	(string-exists-after-cursor (string-match "[^\\\s\\\n\\\t]" (buffer-substring (point) (point-at-eol)))))
+        (string-exists-after-cursor (string-match "[^\\\s\\\n\\\t]" (buffer-substring (point) (point-at-eol)))))
     (cond ((or (eolp)
-	       (not string-exists-after-cursor)
-	       (and string-exists-before-cursor string-exists-after-cursor))
+               (not string-exists-after-cursor)
+               (and string-exists-before-cursor string-exists-after-cursor))
 	   (progn (princ 1) (newline) (indent-according-to-mode)))
-	(t (progn (princ 2) (open-line 1) (indent-according-to-mode))))))
+          (t (progn (princ 2) (open-line 1) (indent-according-to-mode))))))
 
 (define-key global-map (kbd "C-m") 'newline-or-open-line)
