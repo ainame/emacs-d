3f7b77a093cda953c255d1ee0b0d154db35429c0
ainame <s.namai.2012@gmail.com>
2013-11-05 02:48:42 +0900

    add `rake device` command

--- src/el-get-packages-conf/init-motion-mode.el
+++ src/el-get-packages-conf/init-motion-mode.el
@@ -2,4 +2,6 @@
 (add-to-list 'ac-modes 'motion-mode)
 (add-hook 'ruby-mode-hook 'motion-recognize-project)
 (define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
+(define-key motion-mode-map (kbd "C-c C-d") (lambda () (interactive)
+                                              (motion-execute-rake-command "device")))
 (define-key motion-mode-map (kbd "C-c C-o") 'motion-dash-at-point)
