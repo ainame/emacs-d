995545596de28d7041307cd95ee58b8ea62644a5
ainame <s.namai.2012@gmail.com>
2014-01-03 05:00:22 +0900

    bug fix: make be able to compile by rake

--- src/init/my-setup.el
+++ src/init/my-setup.el
@@ -1,7 +1,7 @@
-(require 'my-setup/initialize "initialize")
-(require 'my-setup/el-get-conf "el-get-conf")
-(require 'my-setup/environment "environment")
-(require 'my-setup/load-packages "load-packages")
-(require 'my-setup/load-personal-conf "load-personal-conf")
+(require 'my-setup/initialize "initialize.elc")
+(require 'my-setup/el-get-conf "el-get-conf.elc")
+(require 'my-setup/environment "environment.elc")
+(require 'my-setup/load-packages "load-packages.elc")
+(require 'my-setup/load-personal-conf "load-personal-conf.elc")
 
 (provide 'my-setup)
