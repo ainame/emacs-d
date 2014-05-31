6cfc61157dacbb57d0cb170a25b66e90ab57f7ba
ainame <s.namai.2012@gmail.com>
2014-06-01 04:46:49 +0900

    disabled old git-gutter's setting

--- src/el-get-packages-conf/init-git-gutter.el
+++ src/el-get-packages-conf/init-git-gutter.el
@@ -4,12 +4,12 @@
 (define-key global-map (kbd "C-x v =") 'git-gutter:popup-hunk)
 (define-key global-map (kbd "C-x R") 'git-gutter:revert-hunk)
 
-(defvar git-gutter:my-threshold-of-stopping-active-update 2000)
-(add-hook 'find-file-hook
-          (lambda ()
-            (if (> (line-number-at-pos (point-max))
-                   git-gutter:my-threshold-of-stopping-active-update)
-                (progn
-                  (make-local-variable 'git-gutter:update-hooks)
-                  (setq git-gutter:update-hooks '(after-save-hook after-revert-hook)))
-              nil)))
+;; (defvar git-gutter:my-threshold-of-stopping-active-update 1000)
+;; (add-hook 'find-file-hook
+;;           (lambda ()
+;;             (if (> (line-number-at-pos (point-max))
+;;                    git-gutter:my-threshold-of-stopping-active-update)
+;;                 (progn
+;;                   (make-local-variable 'git-gutter:update-hooks)
+;;                   (setq git-gutter:update-hooks '(after-save-hook after-revert-hook)))
+;;               nil)))
