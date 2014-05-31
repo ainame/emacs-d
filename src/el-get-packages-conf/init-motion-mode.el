6ed99d659c55ad302c2f41db6f06a3f7fb7665ae
ainame <s.namai.2012@gmail.com>
2014-06-01 04:47:12 +0900

    disable motion-flymake

--- src/el-get-packages-conf/init-motion-mode.el
+++ src/el-get-packages-conf/init-motion-mode.el
@@ -1,6 +1,7 @@
 (require 'motion-mode)
 (add-to-list 'ac-modes 'motion-mode)
 (add-hook 'ruby-mode-hook 'motion-recognize-project)
+(setq motion-flymake nil)
 (define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
 (define-key motion-mode-map (kbd "C-c C-d") (lambda () (interactive)
                                               (motion-execute-rake-command "device")))
