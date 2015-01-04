cb32664a383ac58b661d7c2562be884cd169e335
ainame <s.namai.09@gmail.com>
2015-01-05 06:49:41 +0900

    add fall back

--- src/init/my-setup.el
+++ src/init/my-setup.el
@@ -1,7 +1,36 @@
-(require 'my-setup/initialize "initialize.elc")
-(require 'my-setup/el-get-conf "el-get-conf.elc")
-(require 'my-setup/environment "environment.elc")
-(require 'my-setup/load-packages "load-packages.elc")
-(require 'my-setup/load-personal-conf "load-personal-conf.elc")
+(let ((result
+       (ignore-errors
+         (require 'my-setup/initialize "initialize.elc")
+         t)))
+  (unless result
+    (require 'my-setup/initialize "initialize.el")))
+
+(let ((result
+       (ignore-errors
+         (require 'my-setup/el-get-conf "el-get-conf.elc")
+         t)))
+  (unless result
+    (require 'my-setup/el-get-conf "el-get-conf.el")))
+
+(let ((result
+       (ignore-errors
+         (require 'my-setup/environment "environment.elc")
+         t)))
+  (unless result
+    (require 'my-setup/environment "environment.el")))
+
+(let ((result
+       (ignore-errors
+         (require 'my-setup/load-packages "load-packages.elc")
+         t)))
+  (unless result
+    (require 'my-setup/load-packages "load-packages.el")))
+
+(let ((result
+       (ignore-errors
+         (require 'my-setup/load-personal-conf "load-personal-conf.elc")
+         t)))
+  (unless result
+    (require 'my-setup/load-personal-conf "load-personal-conf.el")))
 
 (provide 'my-setup)
