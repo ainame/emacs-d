2ded6e491c40bfd5ed546d3e669b24703fd045d4
ainame <s.namai.09@gmail.com>
2015-07-12 03:39:17 +0900

    unify auto-mode-alist for XXXfile

--- src/el-get-packages-conf/init-ruby-mode.el
+++ src/el-get-packages-conf/init-ruby-mode.el
@@ -1,10 +1,4 @@
-(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Cheffile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Procfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Podfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '(".*file" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
 (setq ruby-deep-indent-paren-style nil)
